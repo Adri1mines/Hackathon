452e569377a85cb0f781557ecbffccffbc4f5e9f
Adrien Le Marchand <adrien.le_marchand@etu.minesparis.psl.eu>
2023-01-27 11:37:20 +0100

    fichier code pour ennemis

---
+++ Ennemis.py
@@ -0,0 +1,103 @@
+import random
+from math import *
+class Ennemy:
+    def __init__(self, hp, atk, position):
+        self.hp = hp
+        self.atk = atk
+        self.position = position
+
+    def search_player(self, player_position):
+        player_x = player_position[0]
+        player_y = player_position[1]
+        x = self.position[0]
+        y = self.position[1]
+        dist = sqrt((player_x - x)**2 + (player_y - y)**2)
+        if dist <= 3:
+            if random.randint(0,1) == 1:
+                if player_x > x:
+                    self.x += 1
+                elif player_x < x:
+                    self.x -= 1
+            else:
+                if player_y > y:
+                    self.y += 1
+                elif player_y < y:
+                    self.y -= 1
+
+    def looted(self):
+        pass
+
+class Bat(Ennemy):
+    def __init__(self, position):
+        super.__init__(6, 1, position)
+
+    def search_player(self, player_position):
+        super.search_player(player_position)
+    
+    def loot(self):
+        super.loot()
+
+    
+class Snake(Ennemy):
+    def __init__(self, position):
+        super.__init__(6, 2, position)
+
+    def search_player(self, player_position):
+        super.search_player(player_position)
+    
+    def loot(self):
+        super.loot()
+
+
+class Nutria(Ennemy):
+    def __init__(self, position):
+        super.__init__(8, 2, position)
+
+    def search_player(self, player_position):
+        super.search_player(player_position)
+    
+    def loot(self):
+        super.loot()
+
+class Squirrel(Ennemy):
+
+    def __init__(self, position):
+        super.__init__(6, 4, position)
+
+    def search_player(self, player_position):
+        super.search_player(player_position)
+    
+    def loot(self):
+        super.loot()
+
+class Minotaur(Ennemy):
+
+    def __init__(self, position):
+        super.__init__(15, 6, position)
+
+    def search_player(self, player_position):
+        super.search_player(player_position)
+    
+    def loot(self):
+        super.loot()
+
+class Dragon(Ennemy):
+
+     def __init__(self, position):
+        super.__init__(25, 15, position)
+
+    def search_player(self, player_position):
+        super.search_player(player_position)
+    
+    def loot(self):
+        super.loot()
+
+    
+
+
+
+
+
+
+
+        
